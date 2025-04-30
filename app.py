d7b2da54201397110db81c438c4bb92da2f6a134
betosantos <oliveira.roberto@gmail.com>
2025-04-30 14:37:21 -0300

    Debug  igual a True

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
-
-
-
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
-
-
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
-
-
+    app.run(debug=True)
