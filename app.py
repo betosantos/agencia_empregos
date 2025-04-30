7aba52e47e3bedef91c2bc0599d4ac5c3cff7d7d
betosantos <oliveira.roberto@gmail.com>
2025-04-30 15:07:11 -0300

    alteraçao simples

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask
 
 app = Flask(__name__)
 
-
-
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
 if __name__ == '__main__':
