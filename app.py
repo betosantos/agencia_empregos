de4f42eb67b2a9af71ab2b90923c5bc7d0d723e5
betosantos <oliveira.roberto@gmail.com>
2025-05-02 09:58:32 -0300

    Configuraçao Inicial

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template
-
+from db import db
+from models import Usuario
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)
 
 
 @app.route('/')
-def hello():
+def index():    
     return render_template('index.html')
 
 
-@app.route('/teste')
-def teste():
-    return 'Teste'
-
-
-
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,28 +1,6 @@
 from db import db
-from flask_login import UserMixin
-from datetime import datetime
 
 
-
-class Usuario(db.Model, UserMixin):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    senha = db.Column(db.String(255), nullable=False)
-
-
-
-class Vaga(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
-    descricao = db.Column(db.Text, nullable=False)
-
-
-class Candidatura(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
-    vaga_id = db.Column(db.Integer, db.ForeignKey('vaga.id'), nullable=False)
-    data_candidatura = db.Column(db.DateTime, default=datetime.utcnow)
-
-    usuario = db.relationship('Usuario', backref='candidaturas')
-    vaga = db.relationship('Vaga', backref='candidaturas')
