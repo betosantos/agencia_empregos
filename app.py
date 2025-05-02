9d8d3316528f2f97b4e9cd27b057b2759b7a9c45
betosantos <oliveira.roberto@gmail.com>
2025-05-02 11:43:20 -0300

    Alteração drop no Bando de daodos

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()  # carrega variáveis do .env
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://agencia_empregos_user:QRYDPIqYwF4TF9VgL9Ss8PKGASkN3fsr@dpg-d0abvjp5pdvs73ckhntg-a.oregon-postgres.render.com/agencia_empregos"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -19,10 +19,15 @@ def index():
     return render_template('index.html')
 
 
+
 if __name__ == '__main__':
     with app.app_context():
+        db.drop_all()    # Apaga todas as tabelas
         db.create_all()
+        print("Tabelas apagadas e recriadas com sucesso!")
     app.run(debug=True)
 else:
     with app.app_context():
+        db.drop_all()    # Apaga todas as tabelas
         db.create_all()
+        print("Tabelas apagadas e recriadas com sucesso!")
