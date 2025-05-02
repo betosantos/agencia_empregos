4dce6190e719615b813a839a8ed9cd527592b6d2
betosantos <oliveira.roberto@gmail.com>
2025-05-02 10:55:12 -0300

    Alteração na tabela Usuario do Bando de daodos correção

--- models.py
+++ models.py
@@ -5,4 +5,4 @@ from db import db
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    senha = db.Column(db.String(255) nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
