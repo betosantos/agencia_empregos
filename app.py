27667dbf0b3f86b9cc582201acc3e329d316a294
betosantos <oliveira.roberto@gmail.com>
2025-05-15 10:53:03 -0300

    alteração no projeto sem acesso ao banco de dados

--- app.py
+++ app.py
@@ -2,20 +2,11 @@ import os
 from flask import Flask, render_template
 from db import db
 from models import Usuario
-from dotenv import load_dotenv
-
-load_dotenv()  # carrega variáveis do .env
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
 
-# Criação das tabelas no deploy
-with app.app_context():    
-    db.create_all()
-    print('Tabelas criadas com sucesso!!')
+
 
 @app.route('/')
 def index():    
